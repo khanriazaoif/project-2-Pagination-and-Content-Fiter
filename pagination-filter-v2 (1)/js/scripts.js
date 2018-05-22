af9ea9dc17073ae082f1d8ab600638f9c0449af3
Riaz Khan <khanriazaoif@gmail.com>
2018-05-21 20:36:43 -0500

    Added li tags to the ul tag inside the div with class 'pagination'

--- pagination-filter-v2 (1)/js/scripts.js
+++ pagination-filter-v2 (1)/js/scripts.js
@@ -52,6 +52,8 @@ function showPage(pageNumber, allStudents) {
     console.log(ate);
     let divUl = document.createElement('ul');
     ate.appendChild(divUl);
+    let li = document.createElement('li');
+    divUl.appendChild(li);
 
     // let divPaginationUl = document.createElement('ul');
     // console.log(divPaginationUl);
@@ -68,3 +70,27 @@ function showPage(pageNumber, allStudents) {
 
     //console.log(pages);
     appendPageLinks(allStudents);
+
+
+
+//     <div class="pagination">
+//     <ul>
+//       <li>
+//         <a class="active" href="#">1</a>
+//       </li>
+//        <li>
+//         <a href="#">2</a>
+//       </li>
+//        <li>
+//         <a href="#">3</a>
+//       </li>
+//        <li>
+//         <a href="#">4</a>
+//       </li>
+//        <li>
+//         <a href="#">5</a>
+//       </li>
+//     </ul>
+//   </div>
+
+    
